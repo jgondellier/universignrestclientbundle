6fcbde3b69018aa054d64f85b367add7b5e8f9c4
jgondellier <jgondellier@yahoo.fr>
2024-03-28 11:34:00 +0100

    Update UniversignRestClientBundle.php

--- UniversignRestClientBundle.php
+++ UniversignRestClientBundle.php
@@ -1,6 +1,6 @@
 <?php
 
-use Universign\Rest\ClientBundle\DependencyInjection\UniversignClientExtension;
+use Universign\Rest\ClientBundle\DependencyInjection\UniversignRestClientExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class UniversignRestClientBundle extends Bundle
@@ -8,7 +8,7 @@ class UniversignRestClientBundle extends Bundle
     public function getContainerExtension()
     {
         if (null === $this->extension) {
-            $this->extension = new UniversignClientExtension();
+            $this->extension = new UniversignRestClientExtension();
         }
         return $this->extension;
     }
