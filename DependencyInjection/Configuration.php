535846afb58ede9c1dc371033f55b773086e92ce
jgondellier <jgondellier@yahoo.fr>
2024-03-29 17:10:17 +0100

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,5 @@
 <?php
-
-namespace DependencyInjection;
+namespace namespace Universign\Rest\ClientBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
